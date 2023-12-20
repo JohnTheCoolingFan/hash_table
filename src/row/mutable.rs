f2522a63625395d6e98770bed00999cfeaabde6a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-12-20 16:31:57 +0300

    Actually implement HashTableMutableBorrowedRow::into_iter

--- src/row/mutable.rs
+++ src/row/mutable.rs
@@ -1,7 +1,10 @@
+//! Mutable borrow column access
+
 use std::borrow::Borrow;
 
 use crate::*;
 
+/// Row of a table with mutable access to the values
 #[derive(Debug)]
 pub struct HashTableMutableBorrowedRow<'t, K, V> {
     pub(crate) indices_table: &'t HashMap<K, usize>,
@@ -9,13 +12,14 @@ pub struct HashTableMutableBorrowedRow<'t, K, V> {
 }
 
 impl<'t, 'r: 't, K, V> HashTableMutableBorrowedRow<'t, K, V> {
-    pub fn get<Q>(&'r mut self, key: &Q) -> Option<&mut V>
+    /// Get an element of this row in the requested `column`.
+    pub fn get<Q>(&'r mut self, column: &Q) -> Option<&mut V>
     where
         K: Borrow<Q>,
         K: Hash + Eq,
         Q: Hash + Eq + ?Sized,
     {
-        self.indices_table.get(key).map(|i| &mut self.values[*i])
+        self.indices_table.get(column).map(|i| &mut self.values[*i])
     }
 }
 
@@ -24,10 +28,16 @@ impl<'t, K, V> IntoIterator for HashTableMutableBorrowedRow<'t, K, V> {
     type IntoIter = HashTableMutableBorrowedRowIntoIter<'t, K, V>;
 
     fn into_iter(self) -> Self::IntoIter {
-        todo!()
+        HashTableMutableBorrowedRowIntoIter {
+            indices_table_iter: self.indices_table.iter(),
+            values: self.values.iter_mut().map(Option::Some).collect(),
+        }
     }
 }
 
+/// Iterator over mutable references to values of a table row.
+///
+/// Returned by [`HashTableMutableBorrowedRow::into_iter`].
 #[derive(Debug)]
 pub struct HashTableMutableBorrowedRowIntoIter<'t, K, V> {
     indices_table_iter: <&'t HashMap<K, usize> as IntoIterator>::IntoIter,
