c53a70cc83d64dfb4cdbe1fb255fb96c1bde3608
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-01-06 12:47:49 +0300

    Automatically copy readme to doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
-//! # Hash Table (data structure)
-//!
-//! This crate provides a 2d data structure where columns are indexed by hashable keys and rows are indexed using an unsigned integer (`usize`).
-//!
-//! ## Features
-//! | Name        | Description                             | Enabled by default? |
-//! |-------------|-----------------------------------------|---------------------|
-//! | `hashbrown` | Uses `hashbrown` instead of std hashmap | No                  |
-//! | `serde`     | Serde trait implementations             | Yes                 |
+#![doc = include_str!("../README.md")]
 
 pub mod column;
 pub mod row;
