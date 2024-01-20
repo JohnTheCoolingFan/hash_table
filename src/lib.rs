b9751c2ac7385967dc67e9e21cb2c7dd0dae0da3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-01-20 18:23:48 +0300

    Error on an invalid feature combination

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 #![doc = include_str!("../README.md")]
 
+#[cfg(all(
+    feature = "serde",
+    feature = "hashbrown",
+    not(feature = "hashbrown-serde")
+))]
+compile_error!("Due to how rust features work, you need to enable the `hashbrown-serde` feature to use both hashbrown and serde");
+
 pub mod column;
 pub mod row;
 pub mod table;
